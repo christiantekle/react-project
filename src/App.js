15b46e58326fc1b72a1789984019efe4a255b924
christiantekle <christiantekle@users.noreply.github.com>
2020-06-14 05:09:08 +0400

    addTodo is add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+import nextId from 'react-id-generator';
 
 import "./App.css"
  
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
   todos: [
     {
-      id: 1,
+      id: nextId(),
       title: 'Take out the trash',
       completed: false
     },
     {
-      id: 2,
+      id: nextId(),
       title: 'Dinner with family',
       completed: false
     },
     {
-      id: 3,
+      id: nextId(),
       title: 'Meeting with boss',
       completed: false
     }
@@ -41,12 +42,21 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
   
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: nextId(),
+      title,
+      completed: false
+    } 
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
   render() {  
       return (
       <div className="App">
         <div className="container">
             <Header />
-            <AddTodo />
+            <AddTodo addTodo={this.addTodo}/>
             <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
         </div>
        </div> 
